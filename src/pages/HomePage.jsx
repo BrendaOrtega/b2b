c8c5170ed6e5d4e084544c33af9d8ab3de67ddc4
Brenda Gonzalez Ortega <brendago@MacBook-Pro-de-Brenda.local>
2022-03-17 18:22:14 -0600

    typing effect & copy updates

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -23,7 +23,11 @@ export default function HomePage() {
 
             <section className={styles.welcome}>
                 <Fade bottom >
-                    <h1 className={styles.header}>El socio perfecto para conocer a tus consumidores</h1>
+                    <h1 className={styles.header}>El socio perfecto para conocer a tus</h1>
+                    <div className={styles.wrapper}>
+                        <div className={styles.staticTxt}></div>
+                        <Typer />
+                    </div>
                     <p className={styles.slogan}>Creamos una experiencia humana y diferente de la investigación de mercado en línea, reclutando y perfilando usuarios con base en los requerimientos de tu estudio para asegurar resultados de calidad.</p>
                     <Link to="/contacto">
                         <button className={styles.buttonPrimary}>Contactar</button>
@@ -88,28 +92,28 @@ export default function HomePage() {
                     <Fade bottom>
                         <div className={styles.cardStep}>
                             <h3>Diseño y programación de encuesta</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet </p>
+                            <p>Programamos tu encuesta y te entregamos la versión Test. Una vez aprobada lanzamos el cuestionario a los panelistas.</p>
                             <img src={arrow} alt="arrow" />
                         </div>
                     </Fade>
                     <Fade bottom>
                         <div className={styles.cardStep}>
                             <h3>Perfilamiento de usuarios</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet </p>
+                            <p>Segmentamos a los panelistas de acuerdo al objetivo de tu proyecto. Enviamos invitaciones de forma paulatina para obtener los mejores resultados. </p>
                             <img src={arrow} alt="arrow" />
                         </div>
                     </Fade>
                     <Fade bottom>
                         <div className={styles.cardStep}>
                             <h3>Supervisión y seguimiento de datos</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet </p>
+                            <p>Realizamos el control de calidad de la base de datos, revisando estratégicamente las respuestas. Te proporcionamos una base preliminar al inicio del campo. </p>
                             <img src={arrow} alt="arrow" />
                         </div>
                     </Fade>
                     <Fade bottom>
                         <div className={styles.cardStep}>
                             <h3>Sintetización de datos y entrega</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet </p>
+                            <p>Nuestro sistema genera gráficas las cuales te compartimos con la base de datos en diferentes formatos que elijas: spss, csv y excel. </p>
                             <img src={arrow} alt="arrow" />
                         </div>
                     </Fade>
@@ -127,5 +131,61 @@ export default function HomePage() {
             </section>
         </section >
 
+    )
+}
+
+export const Typer = ({ cursorClassName, wordClassName, words = ['Consumidores', 'Clientes', 'Usuarios'] }) => {
+    const text = React.useRef('')
+    const index = React.useRef(0)
+
+    let timer
+
+    const deleteWord = () => {
+        clearInterval(timer)
+        const word = text.current.innerText
+        const newWord = word.substring(0, word.length - 1 === -1 ? 1 : word.length - 1)
+        text.current.innerText = newWord
+        if (newWord === '') {
+            clearTimeout(timer)
+            setTimeout(write(words[index.current]), 1000)
+        } else {
+            timer = setTimeout(deleteWord, 100)
+        }
+
+
+    }
+
+    const write = React.useCallback((word = '') => {
+        clearInterval(timer)
+        let counter = -1
+        // teletype.innerHTML = typer.value.substr(0, counter) + cursor;
+        timer = setInterval(() => {
+            counter++
+            text.current.innerText = word.substring(0, counter)
+            if (counter > word.length + 1) {
+                clearInterval(timer)
+                // setTimeout(afterWait, 1000)
+                index.current += 1
+                if (index.current > words.length - 1) {
+                    index.current = 0
+                }
+                setTimeout(deleteWord, 1000)
+                // setTimeout(deleteWord, 1000)
+
+                // setTimeout(() => write(words[index.current]), 1000)
+            }
+        }, 150)
+    }, [])
+
+    React.useEffect(() => {
+        write(words[0])
+    }, [])
+
+    return (
+        <div className={styles.typer}>
+            <span ref={text} className={wordClassName ? wordClassName : styles.word}>
+            </span>
+            <span className={cursorClassName || styles.cursor} ></span>
+        </div>
     )
 }
