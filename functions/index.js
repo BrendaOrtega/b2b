9f64334eac1a974619df6b3cc329b3e17b7770b8
hectorbliss <fixtergeek@gmail.com>
2022-05-02 16:12:59 -0500

    functions added

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
 const functions = require("firebase-functions");
 const nodemailer = require('nodemailer')
-const cors = require('cors')
 
 const transport = nodemailer.createTransport({
     service: 'Gmail',
@@ -8,7 +7,7 @@ const transport = nodemailer.createTransport({
         user: process.env.NODEMAILER_USER,
         pass: process.env.NODEMAILER_PASS,
     },
-})
+});
 
 const sendContactForm = (form) => {
     return transport
@@ -26,19 +25,6 @@ const sendContactForm = (form) => {
         .then((r) => {
             console.log("Accepted => ", r.accepted)
             console.log("Rejected => ", r.rejected)
-            //return transport
-            // .sendMail({
-            // 	subject: "👾🤖¡Alguien nos ha contactado!😎",
-            // 	bcc: ["contacto@fixter.org", "brenda@fixter.org"],
-            // 	html: `
-            // 	<h2> Fecha: ${new Date()} </h2>
-            // 		<h2> Nombre: ${displayName} </h2>
-            // 		<h2> Teléfono: ${tel} </h2>
-            // 		<h2> Email: ${email} </h2>
-            // 		<h2> Mensaje: ${text} </h2>
-
-            // 	`
-            // })
         })
         .catch((e) => console.log(e))
 }
